6946e2f0ee47cea829ea2c8d780ea4aa07fcfa0d
Jonathan Ndambuki <joendambuki16@gmail.com>
2023-03-14 11:41:21 +0300

    Worked with Docker

--- backend/src/Config/index.ts
+++ backend/src/Config/index.ts
@@ -6,7 +6,7 @@ export const sqlConfig = {
   user: process.env.DB_USER as string,
   password: process.env.DB_PWD as string,
   database: process.env.DB_NAME as string ,
-  server: 'localhost',
+  server: 'db',
 
   pool: {
     max: 10,
